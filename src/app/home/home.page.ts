c2e7961f813f1bc1d5f1ad88b1375962509ed9a1
rdwn17 <if21.ridwan@mhs.ubpkarawang.ac.id>
2023-06-10 21:57:12 +0700

    satu

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Geolocation, Geoposition } from '@ionic-native/geolocation/ngx';
+
+
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +11,54 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  latitude:any =0;
+  longitude:any =0;
+  form = {
+    nama : '',
+    nim : '',
+    alamat : ''
 
+  };
+  countrycode: string = "62";
+  number: string = "85155098559";
+  telegramUrl: string = "https://t.me/";
+  telegramUsername: string = "erdwn17";
+  name: string = "";
+  nim: string = "";
+  class: string = "";
+  message: string = "";
+  directUrl: string | undefined;
+  telegram: any;
+
+
+
+
+
+  constructor(
+    private geolocation: Geolocation
+  ) {}
+  sendTele(){
+    const telegramMessage = `Hi, my name is ${this.name}. NIM: ${this.nim}. Class: ${this.class}. ${this.message}`;
+    this.directUrl = `${this.telegramUrl}${this.telegramUsername}?text=${encodeURIComponent(telegramMessage)}`;
+  }
+
+  sendMessage() {
+    const message = 'Hello, this is a test message.';
+    const username = 'erdwn17';
+
+    this.telegram.shareText(message, username)
+      .then(() => {
+        console.log('Message sent successfully.');
+      })
+      .catch((error: any) => {
+        console.log('Error sending message:', error);
+      });
+  }
+  // async getCurrentLocation() {
+  //   try {
+  //     const position = await Geolocation.getCurrentPosition();
+  //     this.latitude = position.coords.latitude;
+  //     this.longitude = position.coords.longitude;
+  //   } catch (error) {}
+  // }
 }
